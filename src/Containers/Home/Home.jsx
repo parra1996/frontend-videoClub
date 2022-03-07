4c935ebf9545ef05ab8279352187341833107aec
“juanP1996” <parrajp1996@gmail.com>
2022-03-07 20:42:02 +0100

    agregando funcionalidad al container login

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,19 +1,42 @@
-import React from "react"; 
 import {useNavigate} from 'react-router-dom';
 import './Home.css'
 
-
-import './Home.css' ; 
+import { useEffect, useState } from 'react';
 
 const Home = () => {
 
     let navigate = useNavigate();
+
+    //Primero comprobamos en el hook si tenemos el token (estamos logeados)
+    const [credentials, setcredentials] = useState(JSON.parse(localStorage.getItem("dataUser")));
+
+    const takeMeLogin = () => {
+        setTimeout(()=>{
+            navigate("/login");
+        },1500)
+    }
+
+    if(credentials?.dataUser?.token !== undefined){
+
+        return(
+            <div>
+                Hola {credentials.dataUser.nombre}, bienvenido a tu app favorita
+            </div>
+        )
+
+    }else{
+        return(
+            <div className='homeDesign'>
+                Hola foraster@, debes de logearte primero....
+                <div className='designBotonHome' onClick={()=>takeMeLogin()}>
+                    LOGIN
+                </div>
+            </div>
+            
+        )
+    }
+
     
-    return (
-        <div className="homeDesign">
-            soy home
-        </div>
-    )
 }
 
 export default Home ; 
--- src/Containers/Login/Login.jsx
+++ src/Containers/Login/Login.jsx
@@ -1,14 +1,133 @@
-import React from "react"; 
 
-import './Login.css' ; 
+import React, {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+
+import './Login.css';
 
 const Login = () => {
 
-    return (
-        <div className="loginDesign">
-            soy Login
-        </div>
-    )
-}
+    let navigate = useNavigate();
 
-export default Login ; 
+    //1-Hooks (equivalen al estado en los componentes de clase)
+    const [credentials, setCredenciales] = useState("");
+    const [datosUsuario, setDatosUsuario] = useState({email: "", password: ""});
+    const [msgError, setMsgError] = useState("");
+    const [msgError2, setMsgError2] = useState("");
+
+    //Funciones handlers
+    const rellenarDatos = (e) => {
+        //Funcion handler que setea los datos en el hook...[e.target.name] obtiene 
+        //el nombre de la propiedad a cambiar, e.target.value tiene el valor..ambos
+        //obtienen los datos del evento, que es el hecho de escribir en un input en concreto
+        setDatosUsuario({...datosUsuario, [e.target.name]: e.target.value})
+    };
+
+    const checkPassword = (e) => {
+
+        if(e.target.value.length < 4){
+            setMsgError("EL password debe de tener 4 caracteres");
+        } else {
+            setMsgError("");
+        }
+
+    };
+
+    //3-useEffect
+
+    // useEffect(()=>{
+    //     //Este useEffect, sólo se ejecuta la primera vez que
+    //     //se monta el componente. Se diferencia por el argumento 
+    //     //de array vacio que hemos puesto
+
+    //     console.log("Me he montado por primera y única vez");
+
+    // },[]);
+
+    useEffect(()=>{
+        //Este useEffect se ejecutará por cada vez que se actualize el 
+        //componente. Es decir, cuando cambie un hook y por lo tanto se actualize el componente.
+
+        //Es peligroso cambiar hooks aqui, si no tenemos condicionales que eviten
+        //que entremos en bucles infinitos.
+        // console.log("Credenciales vale....", credenciales);
+
+        if(credentials?.token !== undefined){
+
+            setTimeout(()=>{
+                navigate("/");
+            }, 3000);
+        };
+
+    });
+
+    //Funciones locales
+
+    const login = async () => {
+
+        try {
+
+            let body = {
+                 email: datosUsuario.email,
+                 password: datosUsuario.password
+            }
+
+            let resultado = await axios.post("https://jppl-bbdd-videoclub.herokuapp.com/usuarios/login",body);
+
+            //Cambiamos el valor del hook credenciales, por lo tanto se recargará el componente
+            if(resultado.data === "Usuario o contraseña inválido"){
+                setMsgError2("Usuario o contraseña inválido")
+            }else{
+
+                setCredenciales(resultado.data);
+            }
+
+
+        } catch (error) {
+
+            console.log(error)
+
+        }
+    };
+
+    const takeMeRegister = () => {
+        setTimeout(()=>{
+            navigate("/register");
+        },1000);
+    }
+    //2-Render (lo que pinta en pantalla)
+
+    if(credentials?.token !== undefined){
+        return(
+            <div>Hola {credentials?.usuario?.name}, bienvenido a Finder, 
+            donde ligar ya no es un milagro.</div>
+        )
+    } else {
+         
+        return(
+            
+            <div className='loginDesign'>
+
+                 {<pre>{JSON.stringify(datosUsuario, null,2)}</pre>}
+
+                <div className="designFormulario">
+                    <input type="email" name="email" id="email" title="email" placeholder="Correo Electrónico" autoComplete="off" onChange={(e)=>{rellenarDatos(e)}}/>
+                    <input type="password" name="password" id="password" title="password" placeholder="Contraseña" autoComplete="off" onChange={(e)=>{rellenarDatos(e); checkPassword(e)}}/>
+                    {msgError}
+                    {msgError2}
+                </div>
+                <div className="loginDesign2 espacio" onClick={()=>login()}>Log in</div>
+                En caso de no estar registrado/a por favor, hazlo.
+                <div className='designBotonHome' onClick={()=>takeMeRegister()}>
+                    REGISTER
+                </div>
+            </div>
+        );
+    }
+    
+
+};
+
+
+export default Login;
