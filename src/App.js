f8d3f4b36481de18f22fb4624f2f6ce1534fdd3c
“juanP1996” <parrajp1996@gmail.com>
2022-03-07 19:48:33 +0100

    agregando endpoints a App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Home from './Containers/Home/Home';
+import Profile from './Containers/Profile/Profile';
+import Register from './Containers/Register/Register';
+import Login from './Containers/Login/Login';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+
+        <Routes>
+          <Route path={'/'} element={<Home/>} />
+          <Route path={'/profile'} element={<Profile/>} />
+          <Route path={'/register'} element={<Register/>} />
+          <Route path={'/login'} element={<Login/>} />
+        </Routes>
+
+      </BrowserRouter>
+
     </div>
   );
 }
--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,12 +1,17 @@
 import React from "react"; 
+import {useNavigate} from 'react-router-dom';
+import './Home.css'
+
 
 import './Home.css' ; 
 
 const Home = () => {
 
+    let navigate = useNavigate();
+    
     return (
-        <div>
-
+        <div className="homeDesign">
+            soy home
         </div>
     )
 }
