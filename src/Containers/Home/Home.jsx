80822dcc8e4dea1f45b2aa8dd7f7b5da0adf6f13
“juanP1996” <parrajp1996@gmail.com>
2022-03-20 23:52:56 +0100

    video home no funcionaa

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -17,8 +17,8 @@ const Home = () => {
             url='https://jp1996.s3.eu-west-3.amazonaws.com/yt1s.io-The+Most+Beautiful+Shots+in+Movie+History+II-(480p).mp4'
             width="100vw"
             height="42.2em"
-            controls
-            playing
+            controls={true}
+            playing={true}
             // id="ZVO99z6eWIzw5Gh61mTmrTMJRIvNPH9F"
             volume={0.6}
          />
